671d2fc410744bc0d7935b2cab61fcb10c21db26
Fred Melchior <fred@MacBook-Air-de-Fred.local>
2023-10-21 19:34:38 -0300

    feat: user profile and user templates

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,19 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProfileViewSet
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+from .views import *
 
 router = DefaultRouter()
 
 router.register("user", UserViewSet, basename="user")
 router.register("profile", ProfileViewSet, basename="profile")
 
-urlpatterns = router.urls
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path("users/", all_users, name="all-users"),
+    path("users/profile/<int:id>/", ProfileView.as_view(), name="user-profile"),
+    path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
+]
+
+urlpatterns += router.urls
